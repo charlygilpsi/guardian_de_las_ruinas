9ee9497caf90663e48beaf764022b471e7de1924
psiadrian <psi.adrianrub@gmail.com>
2026-02-13 20:00:56 +0100

    crea el peronaje del jugador

--- main.py
+++ main.py
@@ -1,12 +1,49 @@
 from personajes import Personaje
-
-#crear personaje
-nombre_personaje = input("ingresa el nombre de tu personaje:_")
-ataque_personaje = input("ingresa el ataque:_")
-defensa_personaje = input("ingresa la defensa:_")
-vida_personaje = input("ingresa la vida:_")
-
-jugador = Personaje(nombre_personaje, vida_personaje, ataque_personaje, defensa_personaje)
+import combate
 
 
-enemigo = Personaje()
+#personaje: nombre, vida, ataque, defensa
+clases = [
+    ["Guerrero", 100, 50, 50],
+    ["Arquero", 50, 70, 30],
+    ["Mago", 50, 150, 10],
+    ["Picaro", 50, 50, 50],
+    ["Paladin", 150, 40, 60]
+]
+
+i = 1
+for clase in clases:
+    print(f"{i}- Clase: {clase[0]}, vida: {clase[1]}, ataque: {clase[2]}, defensa{clase[3]}")
+    i += 1
+
+
+#pedir calse al usuario
+datos_pj = None
+while datos_pj is None:
+    seleccion_clase = input("Introduce la clase de tu PJ:_").capitalize()
+    for clase in clases:
+        if clase[0] == seleccion_clase:
+            datos_pj = clase
+            break  # Detenemos la búsqueda al encontrarlo
+        else:
+            print("Clase no encontrada")
+jugador = Personaje(datos_pj[0], datos_pj[1], datos_pj[2], datos_pj[3])
+
+
+enemigo = Personaje("Enemigo", 150, 50, 5)
+
+
+#ejecucion del juego
+turno_jugador = True
+while jugador.vida > 0 or enemigo.vida > 0:
+    accion_jugador = input("introduce la accion: atacar/defender")
+    accion_enemigo = combate.maquina_ataque()
+
+
+    if turno_jugador:
+        # (atacante, accion_atacante, defensor, accion_defensor
+        combate.turno(jugador, accion_jugador, enemigo, accion_enemigo)
+    else:
+        combate.turno(enemigo, accion_enemigo, jugador, accion_jugador)
+    
+    turno_jugador = not turno_jugador
