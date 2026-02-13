40e79bf62cc0de9e19080aa9942f7336c98778b9
psiadrian <psi.adrianrub@gmail.com>
2026-02-13 15:28:10 +0100

    creacion base de los personajes

---
+++ personajes.py
@@ -0,0 +1,8 @@
+class personaje:
+
+    def __init__(self, nombre, vida, ataque, defensa):
+        self.nombre = nombre
+        self.vida = vida
+        self.ataque = ataque
+        self.defensa = defensa
+
