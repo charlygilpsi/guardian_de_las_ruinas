f6765afcfc814f00e7e081a4a65ff74b729cb04e
psiadrian <psi.adrianrub@gmail.com>
2026-02-13 18:49:07 +0100

    el usuario crea su personaje

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+from personajes import Personaje
+
+#crear personaje
+nombre_personaje = input("ingresa el nombre de tu personaje:_")
+ataque_personaje = input("ingresa el ataque:_")
+defensa_personaje = input("ingresa la defensa:_")
+vida_personaje = input("ingresa la vida:_")
+
+jugador = Personaje(nombre_personaje, vida_personaje, ataque_personaje, defensa_personaje)
 
 
+enemigo = Personaje()
