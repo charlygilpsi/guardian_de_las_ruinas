480f251597858dbe3b68de512b628ed050ac9f03
charlygilpsi <psicarlosgil@gmail.com>
2026-02-13 19:30:37 +0100

    primera aproximación a turno

--- main_c.py
+++ main_c.py
@@ -1,4 +1,5 @@
 from personajes import Personaje
+from combate import turno, maquina_ataque
 from utils import opcion_valida
 import os
 
@@ -11,14 +12,17 @@ guardian = Personaje("Guardián", 200, 20, 15)
 
 personajes = [mago, luchador, picaro, barbaro, clerigo]
 
-# Banderas para bucles
+# Banderas
 fin_del_juego = False
 opcion_personaje_valida = False
 opcion_turno_valida = False
+victoria = False
 
 # Constantes
 CANTIDAD_PERSONAJES = len(personajes)
-ERROR_OPCION_INVALIDA = f"ERROR: Introduzca un número entero entre 1 y {CANTIDAD_PERSONAJES}."
+TEXTO_ELEGIR_OPCION = "Introduzca aquí el número de la opción seleccionada: "
+ERROR_OPCION_PERSONAJE_INVALIDA = f"ERROR: Introduzca un número entero entre 1 y {CANTIDAD_PERSONAJES}."
+ERROR_ATAQUE_DEFENSA = "ERROR: Introduzca un 1 (atacar) o un 2 (defender)."
 
 while not opcion_personaje_valida:
     print("")
@@ -29,12 +33,31 @@ while not opcion_personaje_valida:
         i = i + 1
 
     print("")
-    opcion = input("Introduzca aquí el número del personaje: ")
+    opcion = input(TEXTO_ELEGIR_OPCION)
 
     if not opcion_valida(opcion, CANTIDAD_PERSONAJES):
         os.system("cls")
         print("")
-        print(ERROR_OPCION_INVALIDA)
+        print(ERROR_OPCION_PERSONAJE_INVALIDA)
     else:
         opcion_personaje_valida = True
 
+while not opcion_turno_valida:
+    print("")
+    print("COMBATE CONTRA EL GUARDIÁN")
+    print("")
+    print("Seleccione una acción: ")
+    print("")
+    print("1. Atacar")
+    print("2. Defender")
+    print("")
+    opcion = input(TEXTO_ELEGIR_OPCION)
+    
+    if not opcion_valida(opcion, 1, 2):
+        os.system("cls")
+        print("")
+        print(ERROR_ATAQUE_DEFENSA)
+    else:
+        accion_guardian = maquina_ataque()
+    #    if opcion == "1":
+            
